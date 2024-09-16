e970f029d27a3416e92e9fd514050b6cf7e2ad62
yaaarslv <savenkovyaroslav53@gmail.com>
2024-09-17 01:25:32 +0300

    pupa i lupa

--- src/services/CodeLinkService.ts
+++ src/services/CodeLinkService.ts
@@ -1,12 +1,14 @@
 const crypto = require('crypto');
+const dotenv = require('dotenv');
+dotenv.config({ path: 'env/.env' });
 
 // Здесь вы можете загрузить ключ из переменных окружения или файла
 const algorithm = 'aes-256-cbc';
 const key = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
+const iv = Buffer.from(process.env.ENCRYPTION_IV, 'hex');
 
 export class CodeLinkService {
     static async encrypt(text: string): Promise<string> {
-        const iv = crypto.randomBytes(16); // Генерация нового iv для каждого шифрования
         const cipher = crypto.createCipheriv(algorithm, key, iv);
         let encrypted = cipher.update(text, 'utf8', 'hex');
         encrypted += cipher.final('hex');
